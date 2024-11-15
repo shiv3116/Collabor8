c3766b98fec4b31ad9fdbc163fc700ab3b2a4adb
shiv3116 <60747392+shiv3116@users.noreply.github.com>
2024-11-15 19:14:14 +0530

    Spring Project Initialized

---
+++ collbaor8-Backend/src/main/java/com/collbaor8/Collbaor8Application.java
@@ -0,0 +1,13 @@
+package com.collbaor8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collbaor8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Collbaor8Application.class, args);
+	}
+
+}
---
+++ collbaor8-Backend/src/test/java/com/collbaor8/Collbaor8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collbaor8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collbaor8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
